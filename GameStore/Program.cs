a4042aa727541428527c2ffcc770e9d144104711
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-09-07 21:59:15 +0530

    Remove unused namespaces and update assembly informational version

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,5 +1,3 @@
-using System.Net.WebSockets;
-using GameStore.DTOs;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
--- GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8457a37c3868472facf16825d183ac7a054b130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126ff511b3341b76a75863addc6d7ec9bbf36603")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
