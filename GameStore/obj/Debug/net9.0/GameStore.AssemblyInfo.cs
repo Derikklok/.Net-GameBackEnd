76192ed43cf63ffb2113064a33eabc9bf9217ff8
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-09-18 09:48:42 +0530

    Update binaries and assembly information for versioning consistency

--- GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e82b72754da3c7ced5233a2233296cc768d125d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8260ff1ffef80d9375470fff98dc954ebfa0956d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
